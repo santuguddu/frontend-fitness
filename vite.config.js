fab9f148f581755d7e68317e18533b85933137f4
Santosh N Totad <santoshtotad8984@gmail.com>
2026-05-09 13:40:18 +0530

    Updated code9

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		host: false,
-	},
+  plugins: [react()],
+
+  // IMPORTANT: GitHub Pages base path
+  // Change "frontend-fitness" if your repo name is different
+  base: '/frontend-fitness/',
+
+  server: {
+    host: false,
+  },
 });
